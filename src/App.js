84489a1544fd90cd897fe5c831b52016728b109f
Suvansh Shukla <suvanshshukla@gmail.com>
2019-09-20 18:01:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route , Link } from "react-router-dom";
+import Home from "./Components/Home/Home"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <Route 
+          path="/"
+          exact
+          render={(props)=> (
+          <Home
+          {...props}
+          />)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+        </Route>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
