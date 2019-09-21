63dd999672483d9c29767c299b816705d1d2f5e6
Suvansh Shukla <suvanshshukla@gmail.com>
2019-09-21 12:59:43 +0530

    Navbar added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Carousel } from 'antd';
-
+import {Nav, Navbar, Form, FormControl, Button} from 'react-bootstrap';
 
 function Home() {
     return (
         <div>
-            <Carousel autoplay>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-            </Carousel>
+            <top>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                </Navbar>
+            </top>
         </div>
     )
 }
 
 export default Home;
+//use the bootstrap carousel instead of antd
